1fa6727a48654a863d5c18f8b81132e61f842dbd
GaiaTuven <ggwiese@gmail.com>
2023-12-28 20:34:24 +0200

    added routes, docstring

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-from flask import Flask, render_template, request
-from db_operations import create_table, insert_cv_data, fetch_latest_cv_data
+import sqlite3
+from flask import Flask, render_template
+from db_operations import create_table, fetch_latest_cv_data, load_and_update_database_with_json
 
 app = Flask(__name__)
 
-create_table()  # Create SQLite table on application startup
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    """
+    Render the index page with CV data.
 
-@app.route('/cv', methods=['GET', 'POST'])
+    Returns:
+    flask.Response: The rendered HTML template.
+    """
+    return render_template("index.html")
+
+@app.route('/cv')
 def cv():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        education = request.form['education']
-        experience = request.form['experience']
-
-        insert_cv_data(name, email, education, experience)
-
+    # Fetch the latest CV data from the database
     cv_data = fetch_latest_cv_data()
-
     return render_template('cv.html', cv_data=cv_data)
 
 if __name__ == '__main__':
+    create_table()
+    load_and_update_database_with_json('cv_data.json')
     app.run(debug=True)
